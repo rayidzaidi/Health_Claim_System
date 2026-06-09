dbbc343bf3f7dd5edceca8d18f99c110d94b8c2a
Rayid <rayidzaidi@gmail.com>
2026-06-09 12:47:09 +0500

    Pre deploy commit

--- start.ps1
+++ start.ps1
@@ -1,3 +1,3 @@
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd backend; .\venv\Scripts\Activate.ps1; uvicorn app.main:app --reload"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd backend; .\venv\Scripts\Activate.ps1; python -m uvicorn app.main:app --reload"
 Start-Process powershell -ArgumentList "-NoExit", "-Command", "cd frontend; npm run dev"
 Write-Host "Started Backend and Frontend in separate windows."
